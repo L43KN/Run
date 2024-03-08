282f5af61baba64f049e458b399eec07d0e5b363
L43KN <ailkushawork@gmail.com>
2024-03-08 23:29:31 +0300

    Creating section catalog and making it responsive

--- src/js/script.js
+++ src/js/script.js
@@ -16,4 +16,29 @@ $(document).ready(function(){
             },
           ],
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tabs-item--active)', function() {
+      $(this)
+        .addClass('catalog__tabs-item--active').siblings().removeClass('catalog__tabs-item--active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    function toggleClassesInItem(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(j) {
+          j.preventDefault();
+          $('.catalog__content-item-face').eq(i).toggleClass('catalog__content-item-face--active');
+          $('.catalog__content-item-back').eq(i).toggleClass('catalog__content-item-back--active');
+        })
+      });
+    }
+
+    toggleClassesInItem('.catalog__content-item-face-footer-link');
+    toggleClassesInItem('.catalog__content-item-back-footer-link');
+
+
+
+
+
+
   });
